91018c931756a7c57f72ae49ced29897baac4308
Svhcop <abdulsami7472@gmail.com>
2025-02-23 20:14:33 +0500

    UI changes in development

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-// Enable strict mode and add error boundary
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+  </React.StrictMode>
+)
